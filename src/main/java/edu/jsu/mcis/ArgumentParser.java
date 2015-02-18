9949e48d83d5b505a3007a4cbc3a0dba2213d743
ychen2 <ychen2@jsu.edu>
2015-02-18 11:22:55 -0600

    Refactory

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -1,22 +1,17 @@
 package edu.jsu.mcis;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class ArgumentParser {
     
-    public String[] numArgs;
-    public  int[] intArray;
-    public ArrayList<String> argNames = new ArrayList<String>();
-    public  int argument1, argument2, argument3;
-    public final  Scanner in = new Scanner(System.in);
-    public String userInput = "";
-    public String missingArguments = "";
-    public String unrecognisedArguments = "";
-    public String unrecognisedArgumentNames = "";
-    public String invalidArguments = "";
+    private String[] numArgs;
+    private  int[] intArray;
+    private ArrayList<String> argNames = new ArrayList<String>();
+    private  int argValue1, argValue2, argValue3;
+    private String userInput = "";
     
     public String getUserInput() {
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter three arguments: ");
         try {
             userInput = in.nextLine();
@@ -26,6 +21,10 @@ public class ArgumentParser {
         }
         return userInput;
     }
+	
+	public void setUserInput(String str){
+		
+	}
     
     public void manageInput() {
         if (userInput.isEmpty()) {
@@ -50,9 +49,9 @@ public class ArgumentParser {
         else if (argNames.size() > 3)
             checkUnrecognisedNames();
         else {
-            argument1 = intArray[0];
-            argument2 = intArray[1];
-            argument3 = intArray[2];
+            argValue1 = intArray[0];
+            argValue2 = intArray[1];
+            argValue3 = intArray[2];
         }
     }
 
@@ -72,18 +71,19 @@ public class ArgumentParser {
     
     public int getValue(String s) {
         if (s.equals(argNames.get(0)))
-            return argument1;
+            return argValue1;
         else if (s.equals(argNames.get(1)))
-            return argument2;
+            return argValue2;
         else if (s.equals(argNames.get(2)))
-            return argument3;
+            return argValue3;
         return 0;
     }
 	
     public String checkMissing() {
+        String missingArguments = "";
         if (numArgs.length == 1 || argNames.size() == 1) {
             missingArguments = "width, height";
-            System.out.println(missingError());
+            System.out.println(missingError()+missingArguments);
             return missingArguments;
                         }
         else if (numArgs.length == 2 || argNames.size() == 2){
@@ -95,17 +95,19 @@ public class ArgumentParser {
     }
     
     public String checkUnrecognised() {
+        String unrecognisedArguments = "";
         for (int i=3; i<numArgs.length; i++) {
             unrecognisedArguments += " " + numArgs[i];
         }
-        System.out.println(unrecognisedError());
+        System.out.println(unrecognisedError() + unrecognisedArguments);
         return unrecognisedArguments;
     }
     public String checkUnrecognisedNames() {
+        String unrecognisedArgumentNames = "";
         for (int i=3; i<argNames.size(); i++) {
             unrecognisedArgumentNames += " " + argNames.get(i);
         }
-        System.out.println(unrecognisedNamesError());
+        System.out.println(unrecognisedNamesError() + unrecognisedArgumentNames);
         return unrecognisedArgumentNames;
     }
 
@@ -114,11 +116,11 @@ public class ArgumentParser {
     }
 
     public String missingError() {
-        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: missing arguments: " + missingArguments;
+        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: missing arguments: ";
     }
 
     public String unrecognisedError() {
-        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: unrecognised arguments: " + unrecognisedArguments;
+        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: unrecognised arguments: ";
     }
     
     public String invalidError() {
@@ -126,6 +128,6 @@ public class ArgumentParser {
     }
 
     public String unrecognisedNamesError() {
-        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: unrecognised arguments: " + unrecognisedArgumentNames;
+        return "\nUsage: Java VolumeCalculator length width height \nVolumeCalculator.Java: error: unrecognised arguments: ";
     }
 }
